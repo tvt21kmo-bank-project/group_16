7d893615f6e99fbaa4365db1abd0a8e02728a6d4
eliaspir <elias.pirttikangas@gmail.com>
2021-11-30 18:43:39 +0200

    Restapi yhteys tietokantaan ja /asiakas

---
+++ api/models/asiakas_model.js
@@ -0,0 +1,28 @@
+const db = require('../database');
+
+const asiakas = {
+  getById: function(id, callback) {
+    return db.query('select * from asiakas where idAsiakas=?', [id], callback);
+  },
+  getAll: function(callback) {
+    return db.query('select * from asiakas', callback);
+  },
+  add: function(asiakas, callback) {
+    return db.query(
+      'insert into asiakas (etunimi,sukunimi,osoite,puhnro) values(?,?,?,?)',
+      [asiakas.etunimi, asiakas.sukunimi, asiakas.osoite, asiakas.puhnro],
+      callback
+    );
+  },
+  delete: function(id, callback) {
+    return db.query('delete from asiakas where idAsiakas=?', [id], callback);
+  },
+  update: function(id, asiakas, callback) {
+    return db.query(
+      'update asiakas set etunimi=?,sukunimi=?, osoite=?, puhnro=? where idAsiakas=?',
+      [asiakas.etunimi, asiakas.sukunimi, asiakas.osoite, asiakas.puhnro, id],
+      callback
+    );
+  }
+};
+module.exports = asiakas;
